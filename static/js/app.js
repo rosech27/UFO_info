ea032a10fb860fe4dd3dd4d003ac79af7587bae7
Rose R Chakkoria <Rose.R.Chakkoria@aexp.com>
2019-03-15 19:53:49 -0700

    Initial commit

---
+++ static/js/app.js
@@ -0,0 +1,48 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+
+
+var tbody = d3.select("tbody");
+
+tableData.forEach(UFOdata => {    
+    var row = tbody.append("tr");
+    Object.entries(UFOdata).forEach(function([key, value]) {
+      //console.log(key, value);
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
+  });
+
+
+var filter = d3.select("#filter-btn");
+
+//tbody = tbody.empty();
+
+filter.on("click", function() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  d3.selectAll("td").remove()
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+  
+  var filData = tableData.filter(sighting => sighting.datetime === inputValue);
+    
+  filData.forEach(UFOdata => {    
+    var row = tbody.append("tr");
+    Object.entries(UFOdata).forEach(function([key, value]) {
+      //console.log(key, value);
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
+  });
+});
+
+
